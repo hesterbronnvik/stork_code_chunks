3cbefa529863fbb96e37c6adb1c1d4402fecd3f2
hesterbronnvik <Pitohui1@gmail.com>
2022-10-29 14:32:20 +0200

    cleaned up redundancies

--- last_transmission_identification.R
+++ last_transmission_identification.R
@@ -13,7 +13,7 @@ library(tidyverse)
 setwd("C:/Users/hbronnvik/Documents/stork_code_chunks")
 load("C:/Users/hbronnvik/Documents/loginStored.rdata")
 studies <- c(24442409, 212096177, 76367850, 21231406, 1176017658, 173641633)
-d_thresh <- 5*24*60*60 # the number of seconds in a day threshold for defining having died
+d_thresh <- 60*24*60*60 # the number of seconds in a day threshold for defining having died
 
 # the files that were just created containing the location data of all adults classified 
 # as migrating if moving more than m_thresh in a day and given north or south
@@ -41,94 +41,45 @@ found_birds <- found_birds %>%
 last_known <- lapply(studies, function(x){
   info <- getMovebankAnimals(x, loginStored) %>% 
     filter(sensor_type_id == 653 & individual_id %in% found_birds$individual.id)
-}) %>% reduce(rbind)
-# for birds that have death comments containing a date, extract that information
-# extract comments with patterns of two digits, punctuation, two digits, punctuation, two digits
-death_dates <- last_known[grep("[0-9]{2}[[:punct:]][0-9]{2}[[:punct:]][0-9]{2}",last_known$death_comments),c("individual_id", "death_comments")]
-# extract only the digits and no words
-death_dates$dd <- gsub("\\.|/", "-", gsub("[^0-9./|-]", "", death_dates$death_comments))
-# add a column of just the year month day
-death_dates$date <- str_extract(death_dates$dd,"(20)[0-9][0-9][[:punct:]]\\d{2}[[:punct:]]\\d{2}")
-# or of the day month year
-death_dates$date2 <-str_extract(death_dates$dd,"\\d{2}[[:punct:]]\\d{2}[[:punct:]](20)[0-9][0-9]") 
-# combine those two formats into one date column
-death_dates$date <- as.Date(ifelse(is.na(death_dates$date), as.Date(as.character(death_dates$date2), format = "%d-%m-%y"), as.Date(death_dates$date)), origin = '1970-01-01')
-death_dates$date <- as.Date(ifelse(is.na(death_dates$date), as.Date(as.character(death_dates$date2), format = "%m-%d-%y"), as.Date(death_dates$date)), origin = '1970-01-01')
-# catch the times that years were entered as only two digits and not four
-death_dates$dd <- ifelse(is.na(death_dates$date), gsub("-20", "-2020", death_dates$dd), death_dates$dd)
-death_dates$date2 <-str_extract(death_dates$dd,"\\d{2}[[:punct:]]\\d{2}[[:punct:]](20)[0-9][0-9]") 
-death_dates$date <- as.Date(ifelse(is.na(death_dates$date), as.Date(as.character(death_dates$date2), format = "%d-%m-%y"), as.Date(death_dates$date)), origin = '1970-01-01')
-death_dates <- death_dates[,c("individual_id", "date")]
+}) %>% reduce(rbind)%>% 
+  select(individual_id, death_comments, timestamp_end, local_identifier)
+
+# for birds that have death comments containing a digit date, extract that information
+patterns <- c("(20)[0-9][0-9][[:punct:]]\\d{1,2}[[:punct:]]\\d{1,2}", # yyyy mm dd
+              "\\d{1,2}[[:punct:]]\\d{1,2}[[:punct:]](20)[0-9][0-9]", # dd mm yy, mm dd yyyy, d m yyyy, m d yyyy
+              "[0-9]{2}[[:punct:]][0-9]{2}[[:punct:]][0-9]{2}") # dd mm yy
+
+death_dates <- last_known %>% 
+  # remove km distances because they do not parse but m may be valid 
+  mutate(death_comments = sub("Sept |Sept\\. ", "September ", gsub("\\d{1,2}( km|km)", "", death_comments))) %>% 
+  rowwise() %>% 
+  # extract dates
+  mutate(char_date = gsub("\\.", "-", str_extract(death_comments, patterns[str_detect(death_comments, patterns)])[1]),
+         # format the dates
+         date = parse_date_time(char_date, orders = c("%d-%m-%Y", "%Y-%m-%d"), tz = "UTC"),
+         # be careful of mdy and clean up two digit dates
+         date = as.POSIXct(ifelse(length(grep("/", char_date)) == 1, parse_date_time(char_date, "%m/%d/%Y", tz = "UTC"), 
+                                  ifelse(nchar(char_date) == 8, parse_date_time(char_date, "%d-%m-%y", tz = "UTC"), date)),
+                           tz = "UTC", origin = "1970-01-01")) %>% 
+  ungroup()
+
+death_dates <- death_dates %>% 
+  rowwise() %>% 
+  # the dates that have months
+  mutate(date = as.POSIXct(ifelse(is.na(date), parse_date_time(death_comments, "%d %b %y", tz = "UTC"), date), tz = "UTC", origin = "1970-01-01"))
+
+# give up and do the last group manually because writing code for each typo is too onerous
+deaths <- data.frame(individual_id = c(78032151, 1173989698, 218609200, 173666935, 23465786, 23466747, 24564548, 1176048179, 1573095923, 1576780467, 1576789367),
+                     date = c("2015-09-08", "2020-09-12", "2015-08-03", "2017-06-27", "2015-05-31", "2015-01-24", "2015-02-03", "2021-04-01", "2021-09-11", "2021-09-01", "2021-09-01"))
+
+death_dates <- death_dates %>% 
+  rowwise() %>% 
+  mutate(date = as.POSIXct(ifelse(individual_id %in% deaths$individual_id, deaths$date[which(deaths$individual_id == individual_id)], date), tz = "UTC", origin = "1970-01-01"))
 
 
 # remove those birds from the search and continue
-missing_dates <- last_known %>% 
-  filter(!individual_id %in% death_dates$individual_id)
-
-mno <- data.frame(month = c("January", "February", "March", "April", "May", "June", "July",
-                            "August", "September", "October", "November", "December", "Jan", 
-                            "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec", "Sept"), 
-                  no = c(1:12, 1:12, 9))
-
-md <- missing_dates %>% 
-  # the ones with death comments that have no date in a format caught above
-  filter(nchar(death_comments) > 0) %>% 
-  select("individual_id", "death_comments", "timestamp_end") %>% 
-  rowwise() %>% 
-  # take out the month if one was recorded in the comment
-  mutate(month = str_extract(death_comments, "January|Jan|February|Feb|March|Mar|April|Apr|May|June|July|August|Aug|September|Sept|Sep.|October|Oct|November|Nov|December|Dec"),
-         m = ifelse(!is.na(month), mno$no[which(mno$month == month)], NA),
-         # take out the year
-         year = str_extract(death_comments, "(20)\\d{2}"),
-         date = as.Date(ifelse(paste0(m, year) == paste0(month(timestamp_end), year(timestamp_end)), date(timestamp_end), NA), origin = '1970-01-01')) %>% 
-  ungroup()
-
-# add these dates to the death confirmation group and move on without them
-death_dates <- rbind(death_dates, md[,c("individual_id", "date")]) %>% 
-  drop_na(date)
-
-# with only the ids for which still no date is known, extract other date formats
-md <- md %>% 
-  filter(!individual_id %in% death_dates$individual_id) %>% 
-  mutate(date = as.Date(str_extract(death_comments,"\\d{2}[[:punct:]]\\d{1}[[:punct:]](20)[0-9][0-9]|\\d{1}[[:punct:]]\\d{1}[[:punct:]](20)[0-9][0-9]|\\d{1}[[:punct:]]\\d{2}[[:punct:]](20)[0-9][0-9]"), format = "%d.%m.%y", origin = '1970-01-01'))
-
-# add these dates to the death confirmation group and move on without them
-death_dates <- rbind(death_dates, md[,c("individual_id", "date")]) %>% 
-  drop_na(date)
-
-# now there is no year in the death comment, so only the year from the end timestamp can be used
-md <- md %>% 
-  filter(!individual_id %in% death_dates$individual_id) %>% 
-  mutate(year = year(timestamp_end), 
-         date = as.Date(ifelse(paste0(m, year) == paste0(month(timestamp_end), year(timestamp_end)), date(timestamp_end), NA), origin = '1970-01-01'))
-
-# add these dates to the death confirmation group and move on without them
-death_dates <- rbind(death_dates, md[,c("individual_id", "date")]) %>% 
-  drop_na(date)
-
-# there are only observations when the last timestamp and the comment date are not in the same month
-# or when there is no day in the comment, or when there was no death but transmission stopped
-# fixing these requires manual work because each one is entered differently (typos, etc.)
-# where a date is simply unclear, I have chosen to investigate it again below
-md <- md %>% 
-  filter(!individual_id %in% death_dates$individual_id) %>% 
-  mutate(date = ifelse(individual_id == "77139271", "2016-04-30", 
-                       ifelse(individual_id == "173666935", "2017-06-29", 
-                              ifelse(individual_id == "23466917", "2014-09-13", NA))))
-
-# add these dates to the death confirmation group and move on without them
-death_dates <- rbind(death_dates, md[,c("individual_id", "date")]) %>% 
-  drop_na(date)
-
-# for remaining birds, use the last date
-missing_dates <- last_known %>% 
-  filter(!individual_id %in% death_dates$individual_id) %>% 
-  rowwise() %>% 
-  mutate(timestamp_end = as.POSIXct(sub(".000", "", timestamp_end), tz = "UTC"), 
-         # find a start date a threshold of days before that (now 5)
-         pull_start = gsub("[[:punct:]]| ", "", paste0(timestamp_end - d_thresh, "000")),
-         timestamp_end = gsub("[[:punct:]]| ", "", paste0(timestamp_end, "000"))) %>% 
-  ungroup()
+missing_dates <- death_dates %>% 
+  filter(is.na(date))
 
 
 # download the ACC 
@@ -136,7 +87,7 @@ library(moveACC)
 info <- getMovebankAnimals(studies[1], loginStored) %>% 
   filter(sensor_type_id == 2365683 & individual_id %in% missing_dates$individual_id) %>% 
   select("individual_id", "timestamp_start", "timestamp_end") %>% 
-  mutate(pull_start = gsub("[[:punct:]]| ", "", as.POSIXct(sub(".000", "", info$timestamp_end), tz = "UTC", origin = "1970-01-01") - d_thresh))
+  mutate(pull_start = gsub("[[:punct:]]| ", "", as.POSIXct(sub(".000", "", timestamp_end), tz = "UTC", origin = "1970-01-01") - d_thresh))
 # the last location of the first bird is one year earlier than the last GPS location
 # the second bird has no ACC
 acc_df <- getMovebankNonLocationData(study = studies[1], animalName = info$individual_id[3],
@@ -145,12 +96,64 @@ ACCtimeRange(acc_df, units="days")
 acc_df2 <- acc_df %>% 
   filter(timestamp > (timestamp[n()] - d_thresh))
 ACCtimeRange(acc_df2, units="days")
-BurstSamplingScedule(acc_df2)
-
-PlotAccDataTIME(df=acc_df2, bursts = c(100))
+# BurstSamplingScedule(acc_df2)
+# 
+# PlotAccDataTIME(df=acc_df2, bursts = c(100))
 
 # timestamp_start = info$pull_start[3],
 # timestamp_end = info$timestamp_end[3],
+axesCol = grep("acceleration_axes", names(acc_df2), value=T)
+if(nrow(acc_df2)>0 & length(unique(acc_df2[, axesCol]))==1){
+  accDf_vedba <- acc_df2 %>% 
+    select("timestamp", "event_id") %>% 
+    mutate(n_samples_per_axis = NA, 
+           acc_burst_duration_s=NA, 
+           meanVeDBA=NA,
+           meanODBA=NA)
+  
+  accRawCol <- grep("accelerations_raw", names(acc_df2), value=T)
+  sampFreqCol <- grep("acceleration_sampling_frequency_per_axis", names(acc_df2), value=T)
+  
+  for(j in 1:nrow(acc_df2)){
+    Naxes <- nchar(as.character(acc_df2[j, axesCol]))
+    accMx <- matrix(as.integer(unlist(strsplit(as.character(acc_df2[j, accRawCol]), " "))), ncol=Naxes, byrow = T)
+    n_samples_per_axis <- nrow(accMx)
+    acc_burst_duration_s <- n_samples_per_axis/acc_df2[j, sampFreqCol]
+    if(nchar(acc_df2[j, axesCol])<3){stop("The ACC data have fewer than 3 axes.")}
+    VeDBA <- sqrt((accMx[,1]-mean(accMx[,1]))^2 + (accMx[,2]-mean(accMx[,2]))^2 + (accMx[,3]-mean(accMx[,3]))^2)
+    ODBA <- (accMx[,1]-mean(accMx[,1])) + (accMx[,2]-mean(accMx[,2])) + (accMx[,3]-mean(accMx[,3]))
+    
+    accDf_vedba[j, c("n_samples_per_axis", "acc_burst_duration_s", "meanVeDBA", "meanODBA")] <- c(n_samples_per_axis, acc_burst_duration_s, mean(VeDBA, na.rm=T), mean(ODBA, na.rm=T))
+  }
+}
+
+ggplot(accDf_vedba, aes(timestamp, meanVeDBA, color = meanVeDBA)) +
+  geom_point(alpha = 0.3) +
+  labs(x = "Date", y = "Mean VeDBA per burst") +
+  theme_classic() +
+  theme(legend.position = "none")
+
+diagnostic <- accDf_vedba %>% 
+  group_by(date(timestamp)) %>% 
+  summarize(activity = mean(meanVeDBA))
+
+diagnostic
+
+ggplot(diagnostic, aes(`date(timestamp)`, activity, color = activity)) +
+  geom_point(alpha = 0.3) +
+  labs(x = "Date", y = "Mean VeDBA per burst") +
+  theme_classic() +
+  theme(legend.position = "none")
+
+last_known$timestamp_end[which(last_known$individual_id == unique(acc_df2$individual_id))]
+last_known$death_comments[which(last_known$individual_id == unique(acc_df2$individual_id))]
+
+
+# if the DBA was below the threshold for a given amount of time
+# but if that time is within the last read, then it shows as not having died
+# is that to be expected (not dead) or fixed?
+
+
 
 # calculate DBA
 # if under a threshold, call this animal dead
@@ -176,7 +179,3 @@ PlotAccDataTIME(df=acc_df2, bursts = c(100))
 # nestlings <- nestlings[!nestlings %in% unfinished$individual_id]
 
 
-# Duplicates containing HAE and DOP values exist.
-# Completed the classification of individual 1576780467 in 2021.
-# Duplicates containing HAE and DOP values exist.
-# Completed the classification of individual 1578616761 in 2022.
